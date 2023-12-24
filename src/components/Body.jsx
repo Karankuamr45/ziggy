74b48edde98a4f3d0ec89fd32ccbccf7dc5fdc98
karan <karankumar46226@gmail.com>
2023-12-24 15:20:36 +0530

    cors solu

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -22,7 +22,7 @@ const Body=()=>{
     },[])
 
     const fetchData=async()=>{
-        const data= await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7100319&lng=77.2603927&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const data= await fetch('https://proxy.cors.sh/https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7100319&lng=77.2603927&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
         const json= await data.json()
         const Restaurants=json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
         setFilterList(Restaurants)
